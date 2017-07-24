16929c50e1b597d25c7f04a3e25c61829cdfd7f4
Andrew Cowden <am.cowden.97@gmail.com>
2017-07-24 11:08:30 -0500

    Added Basic Implementation

--- GameEngine/Piece.java
+++ GameEngine/Piece.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ /*Creator: Andrew Cowden
+ Project Title: Chess-Application
+ Date Created: 07/19/2017
+ Date Last Modified: 07/24/2017  
  */
+
+/*----------Class Overview------------
+This class servers as an abstract class that represents a generic chess piece. 
+It contains thebasis for the validation of piece movement based on the piece type as well 
+as methods to get the color of the piece and other aspects of the general piece definition.*/
+
 package GameEngine;
 
-/**
- *
- * @author amcowden97
- */
-public class Piece {
+public abstract class Piece{  
+    private final boolean pieceColor; 
+    private final Coordinate initialLocation; 
+    private Coordinate currentLocation;  
     
+    public Piece(pieceColor, initialLocation){  
+        this.pieceColor = pieceColor;  
+        this.initialLocation = initialLocation;
+    }
+    
+    public boolean getPieceColor(){
+         return this.pieceColor;
+    }  
+    
+    //Abstract Piece Methods public abstract getPieceDescription();
+    public abstract getPieceDescription();
+    
+    public abstract move(Coordinate newLocation);
+    
+    private abstract validatePieceMove(Coordinate newLocation);} 
 }
+
