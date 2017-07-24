42e9fe6602e3e1ef776c61ff4991c092a780fedc
Andrew Cowden <am.cowden.97@gmail.com>
2017-07-24 12:56:26 -0500

    Added Basic Elements to Class

--- GameEngine/GameBoard.java
+++ GameEngine/GameBoard.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package GameEngine;
 
-/**
- *
- * @author amcowden97
- */
 public class GameBoard {
+    public final int BOARD_LENGTH = 8;
+    private Piece[][] chessArray;            //Represents chess board and piece location
+    private PieceGenerator arrayInit;        //Fills chess board array with pieces
+    private Coordinate whiteKingLocation;    //Used for Check and Checkmate methods
+    private Coordinate blackKingLocation;    //Used for Check and Checkmate methods
     
+    public GameBoard(){
+        this.arrayInit = new PieceGenerator();
+        this.chessArray = arrayInit.initializeChessArray();
+        this.whiteKingLocation = new Coordinate(0,0);   //NEEDS TO BE CHANGED
+        this.blackKingLocation = new Coordinate(0,0);   //NEEDS TO BE CHANGED
+    }
+    
+    public Piece getChessPiece(Coordinate location){
+        return chessArray[location.getX()][location.getY];
+    }
 }
