68e892fac93d1d9130ae4db177bde9d6fcebdd9d
Andrew Cowden <am.cowden.97@gmail.com>
2017-08-06 13:05:30 -0500

    Update Header and Basic Methods

--- GameEngine/Bishop.java
+++ GameEngine/Bishop.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Creator: Andrew Cowden
+  Project Title: Chess-Application
+  Date Created: 07/19/2017
+  Date Last Modified: 08/06/2017
+*/
+
+/*----------Class Overview------------
+This class servers as a representation of the chess piece Bishop. Within this class are methods and attributes to represent
+the possible movement patterns as well as storage for objects of the type Bishop. Note that this class only generates the
+possible movement patterns and does not do movement validation. That is done through the GameBoard class. 
+*/
+import java.util.ArrayDeque;
 package GameEngine;
 
-/**
- *
- * @author amcowden97
- */
-public class Bishop {
+public class Bishop extends Piece {
     
+    public Bishop(boolean color, Coordinate initialLocation){
+        super(color, initialLocation);
+    }
+    
+    
+    public ArrayDeque<Coordinate> getMovementPatterns(){
+        
+    }
 }
